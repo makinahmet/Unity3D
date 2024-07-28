6d6d709011c78e95cffd49150838750d4292bf00
Ahmet MISIR <81827329+makinahmet@users.noreply.github.com>
2024-07-29 01:04:45 +0300

    Update samplePlayerPrefsClass.cs

--- samplePlayerPrefsClass.cs
+++ samplePlayerPrefsClass.cs
@@ -4,7 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class ambPlayerPrefs
+public static class samplePlayerPrefsClass
 {
     public static string[] drivingModes = {"buttonControl", "wheelControl"};
 
